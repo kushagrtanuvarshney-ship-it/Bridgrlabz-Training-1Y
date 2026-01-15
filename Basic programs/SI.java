007717112805f5f6f1b52a87f15c03f673e4b097
Kushagr <kushagrtanuvarshney@gmail.com>
2026-01-15 12:19:03 +0530

    [Kushsgr Varshney] basic programming file added

---
+++ Basic programs/SI.java
@@ -0,0 +1,11 @@
+class SI
+{
+    public static void main(String args[])
+	    {
+		int principle=4;
+		double rate=3;
+		int time=10;
+		double SI = principle*rate*time;
+		System.out.println("SI is="+ SI);
+		}
+}
---
+++ Basic programs/areaofcircle.java
@@ -0,0 +1,10 @@
+class area
+{
+    public static void main(String args[])
+	    {
+		int r=4;
+		double p=3.14;
+		double area = p*r*r;
+		System.out.println("area of circle"+area);
+		}
+}
