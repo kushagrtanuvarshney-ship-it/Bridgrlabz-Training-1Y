56f84f73b438aeb45aae1e6c4e919b2fc45bf6e2
Kushagr <kushagrtanuvarshney@gmail.com>
2026-03-17 16:06:28 +0530

    Removed unwanted files

--- AdvanceTopic1/src/com/gla/Abstraction/Student.java
+++
@@ -1,8 +0,0 @@
-package com.gla.Abstraction;
-
-public class Student extends Teacher{
-    public void info ()
-    {
-        System.out.println("my name is Kushagr");
-    }
-}
--- AdvanceTopic1/src/com/gla/Abstraction/Teacher.java
+++
@@ -1,9 +0,0 @@
-package com.gla.Abstraction;
-public abstract class Teacher{
-    public abstract void info();
-
-    public void position()
-    {
-        System.out.println("PM");
-    }
-}
